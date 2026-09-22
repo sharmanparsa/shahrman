14ce0a321450a9095b4328a5eb617768dbb35df2
sharmanparsa <sadqparsa79@gmail.com>
2026-09-22 20:21:10 +0330

    Delete webapp/locked.js

--- webapp/locked.js
+++
@@ -1,5 +0,0 @@
-// Locked building asset manifest. Images are served from /webapp/assets.
-const LOCKED_BUILDING_ASSETS = {
-  university:'layer2_university_full_locked.png', police:'layer2_police_full_locked.png', stadium:'layer2_stadium_full_locked.png', school:'layer2_school_full_locked.png', hospital:'layer2_hospital_full_locked.png', fire:'layer2_fire_full_locked.png', power:'layer2_power_full_locked.png', industry:'layer2_industry_full_locked.png', park:'layer2_park_full_locked.png', roads:'layer2_roads_full_locked.png', port:'layer2_port_full_locked.png', housing:'layer2_housing_full_locked.png', shopping:'layer2_shopping_full_locked.png'
-};
-

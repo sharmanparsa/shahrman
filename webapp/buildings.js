26a8e5e929cb260b98ffe9bf04c9d4742af1e190
sharmanparsa <sadqparsa79@gmail.com>
2026-09-22 20:21:01 +0330

    Delete webapp/buildings.js

--- webapp/buildings.js
+++
@@ -1,4 +0,0 @@
-// Building asset manifest. Images are served from /webapp/assets.
-const BUILDING_ASSETS = {
-  university:'layer2_university_full.png', police:'layer2_police_full.png', stadium:'layer2_stadium_full.png', school:'layer2_school_full.png', hospital:'layer2_hospital_full.png', fire:'layer2_fire_full.png', power:'layer2_power_full.png', industry:'layer2_industry_full.png', park:'layer2_park_full.png', roads:'layer2_roads_full.png', port:'layer2_port_full.png', housing:'layer2_housing_full.png', shopping:'layer2_shopping_full.png'
-};
